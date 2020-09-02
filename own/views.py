529f26498b236de6a22e07b94102461c8ace3aec
nafi31 <54233303+nafi31@users.noreply.github.com>
2020-09-02 11:57:17 -0700

    Add files via upload

---
+++ own/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Title, Item
+from .forms import CreateList
+from django.template import loader
+
+def create(response):
+    
+    if response.method=="POST":
+        form = CreateList(response.POST)
+        if form.is_valid():
+            name = form.cleaned_data["title"]
+            n = Title(name=name)
+            content = form.cleaned_data["content"]
+            c = Item(text=content,todolist=n)
+            n.save()
+            c.save()
+            name=""
+            content=""
+    else:
+        form = CreateList()
+    return render(response, 'own/create.html', {"form":form})
+
+def notes(request):
+    data= Title.objects.all()
+    tmp= loader.get_template('own/notes.html')
+    context = {
+        'data': data
+    }
+    return HttpResponse(tmp.render(context,request))
+def list(request,id):
+    jk = Title.objects.get(id=id)
+    tmpo= loader.get_template('own/x.html')
+    context = {
+        'jk': jk
+        }
+    return HttpResponse(tmpo.render(context,request))
