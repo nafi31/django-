b5c004bd31f82414a90c823454b1c450d3021476
nafi31 <54233303+nafi31@users.noreply.github.com>
2020-09-02 11:54:06 -0700

    Add files via upload

---
+++ own/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Title ,Item
+class CreateList(forms.ModelForm):
+    title = forms.CharField(label="Title",max_length=100,widget=forms.TextInput(
+        attrs={
+            "class":'form-control',
+        }
+    ))
+    content = forms.CharField(label="Content",max_length=10000,widget=forms.TextInput(
+        attrs={
+            "class":'form-control form-control-lg',
+        }
+    ))
+  
+
+    class Meta:
+        model = Item
+        fields = ('content',)
+        models = Title
+        fields = ('title',)
